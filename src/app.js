03a7f7da20fe83b5a64a2273f21eba2aa4a6888f
Di3goMtz <76271328+Di3goMtz@users.noreply.github.com>
2024-06-10 22:51:56 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use('/api/reports', reportRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
