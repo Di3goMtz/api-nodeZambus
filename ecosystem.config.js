16fac9dac4262c83e73ad1c35e11453f259d0f41
diego <diegomartinezcortes2002@gmail.com>
2024-06-11 21:17:31 -0600

    Mas correciones

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "app",
-        script: "./app.js",
+        script: "./src/app.js",
         env: {
           NODE_ENV: "production",
         },
